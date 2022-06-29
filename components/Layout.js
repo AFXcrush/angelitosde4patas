3d78a851e525d763f23c4ace967cc6201e2373a8
Afx <afxcrush@gmail.com>
2022-06-29 02:24:38 -0500

    Analytics added to head

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 
 import styles from "../styles/Home.module.css";
 import MenuBar from "./MenuBar";
@@ -8,11 +9,30 @@ export default function Layout({ title, children }) {
     <div className={styles.layout}>
       <Head>
         <title>{title}</title>
+
         <meta
           name="Albergue Angelitos de 4 Patas"
           content="Página web del albergue de perros 'Angelitos de 4 Patas' ubicado en S.M.P. Lima, Perú"
         ></meta>
+
         <link rel="icon" href="/favicon.ico" />
+
+        {/* GOOGLE ANALYTICS */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-JCR5Q5BWZW"
+          strategy="afterInteractive"
+          async
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-JCR5Q5BWZW');
+        `}
+        </Script>
       </Head>
 
       <MenuBar />
