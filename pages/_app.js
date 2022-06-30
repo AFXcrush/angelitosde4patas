99264d43ef8c2f134831bec34682c3edbe4eef76
Afx <afxcrush@gmail.com>
2022-06-30 10:32:49 -0500

    back to basic myApp

--- pages/_app.js
+++ pages/_app.js
@@ -5,15 +5,6 @@ import { useRouter } from "next/router";
 
 import * as ga from "../libs/google-analytics";
 
-// function MyApp({ Component, pageProps }) {
-//   return <Component {...pageProps} />
-// }
-
-// export default MyApp
-
-import { ApolloProvider } from "@apollo/client";
-import client from "../libs/apollo";
-
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
@@ -28,8 +19,9 @@ function MyApp({ Component, pageProps }) {
       router.events.off("routeChangeComplete", handleRouteChange);
     };
   }, [router.events]);
+
   return (
-    <ApolloProvider client={client}>
+    <>
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
@@ -43,8 +35,46 @@ function MyApp({ Component, pageProps }) {
           gtag('config', "${process.env.GOOGLE_ANALYTICS_ID}");`}
       </Script>
       <Component {...pageProps} />
-    </ApolloProvider>
+    </>
   );
 }
+
+// export default MyApp
+
+// import { ApolloProvider } from "@apollo/client";
+// import client from "../libs/apollo";
+
+// function MyApp({ Component, pageProps }) {
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     const handleRouteChange = (url) => {
+//       ga.pageview(url);
+//     };
+
+//     router.events.on("routeChangeComplete", handleRouteChange);
+
+//     return () => {
+//       router.events.off("routeChangeComplete", handleRouteChange);
+//     };
+//   }, [router.events]);
+//   return (
+//     <ApolloProvider client={client}>
+//       <Script
+//         strategy="afterInteractive"
+//         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+//       />
+
+//       <Script id="google-analytics-script" strategy="afterInteractive">
+//         {`window.dataLayer = window.dataLayer || [];
+//           function gtag(){window.dataLayer.push(arguments);}
+//           gtag('js', new Date());
+
+//           gtag('config', "${process.env.GOOGLE_ANALYTICS_ID}");`}
+//       </Script>
+//       <Component {...pageProps} />
+//     </ApolloProvider>
+//   );
+// }
 
 export default MyApp;
